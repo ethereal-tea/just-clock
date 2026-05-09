b7e40a434358ed6528979e7e073a8a9fd4be8ee7
ethereal_tea <anthonysedov72@gmail.com>
2026-05-09 18:03:19 +0300

    add digits

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const hourArrow = document.querySelector('#hours')
 const minuteArrow = document.querySelector('#minutes')
 const secondArrow = document.querySelector('#seconds')
+const time = document.querySelector('.time')
 
 const getDate = () =>{
   const date = new Date()
@@ -19,6 +20,7 @@ const updateClock = () => {
   hourArrow.style.rotate = `${(hours + (hours / 60)) * 30}deg`
   minuteArrow.style.rotate = `${minutes * 6}deg`
   secondArrow.style.rotate = `${seconds * 6}deg`
+  time.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
 }
 
 setInterval(updateClock, 1000)
