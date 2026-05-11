6bed03249ceb92d0d35471e823a54df219d4b71b
ethereal_tea <anthonysedov72@gmail.com>
2026-05-11 15:12:41 +0300

    Add weather

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+import './weather'
+
 const hourArrow = document.querySelector('#hours')
 const minuteArrow = document.querySelector('#minutes')
 const secondArrow = document.querySelector('#seconds')
 const time = document.querySelector('.time')
 
-const getDate = () =>{
+const getDate = () => {
   const date = new Date()
   const hours = date.getHours()
   const minutes = date.getMinutes()
@@ -16,7 +18,7 @@ const getDate = () =>{
 }
 
 const updateClock = () => {
-  const {hours, minutes, seconds} = getDate()
+  const { hours, minutes, seconds } = getDate()
   hourArrow.style.rotate = `${(hours + (minutes / 60)) * 30}deg`
   minuteArrow.style.rotate = `${minutes * 6}deg`
   secondArrow.style.rotate = `${seconds * 6}deg`
@@ -24,3 +26,7 @@ const updateClock = () => {
 }
 
 setInterval(updateClock, 1000)
+
+navigator.geolocation.getCurrentPosition((position) => {
+  console.log(position.coords.latitude, position.coords.longitude)
+})
---
+++ src/weather.js
@@ -0,0 +1,57 @@
+class Weather {
+
+    constructor() {
+        this.weatherInfo = document.querySelector('.weather__info')
+        this.weatherCity = document.querySelector('.weather__city-name')
+        this.weatherTemp = document.querySelector('.weather__temp-value')
+        this.weatherDesc = document.querySelector('.weather__city-desc')
+        this.weaherTempFeelsLike = document.querySelector('.weather__fells-like-value')
+        this.wheatherWindSpeed = document.querySelector('.weather__wind-value')
+        this.getLocation()
+        console.log(this.weatherDesc)
+    }
+
+    getLocation() {
+        navigator.geolocation.getCurrentPosition((position) => {
+            this.addListeners(position.coords.latitude, position.coords.longitude)
+        }, (err) => {
+            console.log('Не удалось получить геолокацию, ошибка: ' + err.message)
+        })
+    }
+
+    async getWeatherData(lat, lon) {
+        try {
+            const url = this.getUrl(lat, lon)
+            const response = await fetch(url)
+            return await response.json()
+        }
+
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    async addListeners(lat, lon) {
+        this.weatherData = await this.getWeatherData(lat, lon)
+        this.renderWeatherInfo()
+    }
+
+    renderWeatherInfo() {
+        this.weatherInfo.classList.remove('hidden')
+        this.weatherCity.textContent = this.weatherData.name
+        this.weatherTemp.textContent = `${Math.floor(this.weatherData.main.temp - 273.15)} °C`
+        this.weatherDesc.textContent = this.weatherData.weather[0].main
+        this.weaherTempFeelsLike.textContent = `${Math.floor(this.weatherData.main.feels_like - 273.15)} °C`
+        this.wheatherWindSpeed.textContent = `${this.weatherData.wind.speed} m/s`
+    }
+
+    getUrl(lat, lon) {
+        const apiKey = 'f4134a1e0dee84b7ff1122a96055a4ff'
+        return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+    }
+}
+
+new Weather()
+
+
+// https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f4134a1e0dee84b7ff1122a96055a4ff
