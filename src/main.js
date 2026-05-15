b8109429d91b8d712e2ef2ae53a8cbe471f6d683
ethereal_tea <anthonysedov72@gmail.com>
2026-05-15 18:53:03 +0300

    Redesign

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './weather.js'
 const hourArrow = document.querySelector('#hours')
 const minuteArrow = document.querySelector('#minutes')
 const secondArrow = document.querySelector('#seconds')
-const time = document.querySelector('.time')
+const time = document.querySelector('.clock__digital')
 
 const getDate = () => {
   const date = new Date()
